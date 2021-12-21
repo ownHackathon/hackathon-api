a858e55f03bbee7b54d25248fc16b9a4c6e13b25
BibaltiK <github@exdrals.de>
2021-12-21 06:21:53 +0100

    add unit Test for UserService

--- test/App/Service/UserServiceFactoryTest.php
+++ test/App/Service/UserServiceFactoryTest.php
@@ -10,7 +10,7 @@ use Psr\Container\ContainerInterface;
 
 class UserServiceFactoryTest extends TestCase
 {
-    public function testCanCreateUserService()
+    public function testCanCreateUserService(): void
     {
         $userTable = $this->createMock(UserTable::class);
         $hydrator = $this->createMock(ReflectionHydrator::class);
--- test/App/Service/UserServiceTest.php
+++ test/App/Service/UserServiceTest.php
@@ -9,24 +9,122 @@ use PHPUnit\Framework\TestCase;
 
 class UserServiceTest extends TestCase
 {
-    private UserService $service;
-
-    protected function setUp(): void
+    public function testCanNotCreateUserWithExistUser(): void
     {
-        $userTable = $this->createMock(UserTable::class);
+        $table = $this->createMock(UserTable::class);
         $hydrator = $this->createMock(ReflectionHydrator::class);
 
-        $this->service = new UserService($userTable, $hydrator);
+        $user = new User();
+        $user->setName('fakeName');
+        $user->setEmail('fake@example.com');
 
-        parent::setUp();
+        $table->expects($this->once())
+            ->method('findByName')
+            ->with('fakeName')
+            ->willReturn(['name' => 'fakeName']);
+
+        $hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($user);
+
+        $service = new UserService($table, $hydrator);
+
+        $insert = $service->create($user);
+
+        $this->assertSame(false, $insert);
     }
 
-    public function testCanCreateUser()
+    public function testCanNotCreateUserWithExistEmail(): void
     {
-        $user = new User();
+        $table = $this->createMock(UserTable::class);
+        $hydrator = $this->createMock(ReflectionHydrator::class);
 
-        $insert = $this->service->create($user);
+        $user = new User();
+        $user->setName('fakeName');
+        $user->setEmail('fake@example.com');
+
+        $table->expects($this->once())
+            ->method('findByName')
+            ->with('fakeName')
+            ->willReturn(false);
+
+        $table->expects($this->once())
+            ->method('findByEmail')
+            ->with('fake@example.com')
+            ->willReturn(['email' => 'fake@example.com']);
+
+        $hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($user);
+
+        $service = new UserService($table, $hydrator);
+
+        $insert = $service->create($user);
+
+        $this->assertSame(false, $insert);
+    }
+
+    public function testCanCreateUser(): void
+    {
+        $table = $this->createMock(UserTable::class);
+        $hydrator = $this->createMock(ReflectionHydrator::class);
+
+        $user = new User();
+        $user->setName('fakeName');
+        $user->setEmail('fake@example.com');
+
+        $table->expects($this->once())
+            ->method('findByName')
+            ->with('fakeName')
+            ->willReturn(false);
+
+        $table->expects($this->once())
+            ->method('findByEmail')
+            ->willReturn(false);
+
+        $service = new UserService($table, $hydrator);
+
+        $insert = $service->create($user);
 
         $this->assertSame(true, $insert);
+    }
+
+    public function testFindByIdThrowException(): void
+    {
+        $table = $this->createMock(UserTable::class);
+        $hydrator = $this->createMock(ReflectionHydrator::class);
+
+        $table->expects($this->once())
+            ->method('findById')
+            ->willReturn(false);
+
+        $service = new UserService($table, $hydrator);
+
+        $this->expectException('InvalidArgumentException');
+
+        $service->findById(1);
+    }
+
+    public function testCanFindById(): void
+    {
+        $table = $this->createMock(UserTable::class);
+        $hydrator = $this->createMock(ReflectionHydrator::class);
+        $user = new User();
+        $user->setId(1);
+
+        $table->expects($this->once())
+            ->method('findById')
+            ->with(1)
+            ->willReturn(['id' => 1]);
+
+        $hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($user);
+
+        $service = new UserService($table, $hydrator);
+
+        $user = $service->findById(1);
+
+        $this->assertInstanceOf(User::class, $user);
     }
 }
