a086f99bd538ae1b5ce43b076592c9979a51707e
BibaltiK <github@exdrals.de>
2021-12-22 23:55:22 +0100

    add unit Test for ProjectServiceFactory

---
+++ test/App/Service/ProjectServiceFactoryTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Hydrator\ReflectionHydrator;
+use App\Table\ProjectTable;
+use Laminas\Hydrator\Strategy\DateTimeFormatterStrategy;
+use Psr\Container\ContainerInterface;
+
+class ProjectServiceFactoryTest extends AbstractServiceTest
+{
+    public function testCanCreateProjectService(): void
+    {
+        $table = $this->createMock(ProjectTable::class);
+        $strategy = new DateTimeFormatterStrategy();
+        $container = $this->createMock(ContainerInterface::class);
+
+        $container->expects($this->exactly(3))
+            ->method('get')
+            ->will(
+                $this->returnValueMap(
+                    [
+                        [ProjectTable::class, $table],
+                        [ReflectionHydrator::class, $this->hydrator],
+                        [DateTimeFormatterStrategy::class, $strategy],
+                    ]
+                )
+            );
+
+        $projectServiceFactory = new ProjectServiceFactory();
+
+        $projectService = $projectServiceFactory($container);
+
+        $this->assertInstanceOf(ProjectService::class, $projectService);
+    }
+}
