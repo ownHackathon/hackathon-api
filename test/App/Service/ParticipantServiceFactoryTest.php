0f4eda12eb036565f2cdd72de0068db4576adb34
BibaltiK <github@exdrals.de>
2021-12-24 00:06:08 +0100

    add unit Test for ParticipantServiceFactory

---
+++ test/App/Service/ParticipantServiceFactoryTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Hydrator\ReflectionHydrator;
+use App\Table\ParticipantTable;
+use Laminas\Hydrator\Strategy\DateTimeFormatterStrategy;
+use Psr\Container\ContainerInterface;
+
+class ParticipantServiceFactoryTest extends AbstractServiceTest
+{
+    public function testCanCreateParticipantService(): void
+    {
+        $table = $this->createMock(ParticipantTable::class);
+        $container = $this->createMock(ContainerInterface::class);
+
+        $container->expects($this->exactly(3))
+            ->method('get')
+            ->will(
+                $this->returnValueMap(
+                    [
+                        [ParticipantTable::class, $table],
+                        [ReflectionHydrator::class, $this->hydrator],
+                        [DateTimeFormatterStrategy::class, new DateTimeFormatterStrategy()],
+                    ]
+                )
+            );
+
+        $factory = new ParticipantServiceFactory();
+
+        $service = $factory($container);
+
+        $this->assertInstanceOf(ParticipantService::class, $service);
+    }
+}
