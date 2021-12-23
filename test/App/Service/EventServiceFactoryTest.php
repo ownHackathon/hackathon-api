eccc8779d04a5bcded2a934813fc25fba41114db
BibaltiK <github@exdrals.de>
2021-12-24 00:58:26 +0100

    add unit Test for EventServiceFactory

---
+++ test/App/Service/EventServiceFactoryTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Hydrator\ReflectionHydrator;
+use App\Table\EventTable;
+use Laminas\Hydrator\Strategy\DateTimeFormatterStrategy;
+use Psr\Container\ContainerInterface;
+
+class EventServiceFactoryTest extends AbstractServiceTest
+{
+    public function testCanCreateEventService(): void
+    {
+        $table = $this->createMock(EventTable::class);
+        $container = $this->createMock(ContainerInterface::class);
+
+        $container->expects($this->exactly(3))
+            ->method('get')
+            ->will(
+                $this->returnValueMap(
+                    [
+                        [EventTable::class, $table],
+                        [ReflectionHydrator::class, $this->hydrator],
+                        [DateTimeFormatterStrategy::class, new DateTimeFormatterStrategy()],
+                    ]
+                )
+            );
+
+        $factory = new EventServiceFactory();
+
+        $service = $factory($container);
+
+        $this->assertInstanceOf(EventService::class, $service);
+    }
+}
