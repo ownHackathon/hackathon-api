60bf3ddc4450189607b029bca60ea08c9c63e85c
BibaltiK <github@exdrals.de>
2022-01-26 15:37:41 +0100

    change configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
 
 module.exports = {
-	entry: './build/js/index.js',
+    entry: './build/js/index.js',
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public/assets/js')
-	}
+        filename: 'bundle.js', path: path.resolve(__dirname, 'public/assets/js')
+    }
 };
