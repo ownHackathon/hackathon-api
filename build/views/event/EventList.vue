0005fb97c3231eaa89402ad54fa3c72b5325308c
BibaltiK <github@exdrals.de>
2022-04-07 10:37:55 +0200

    correct columns display

--- build/views/event/EventList.vue
+++ build/views/event/EventList.vue
@@ -1,33 +1,30 @@
 <template>
   <p class="py-6">Hier bekommt ihr eine Übersicht über alle derzeit laufenden und bereits abgeschlossenen Events</p>
   <div class="relative overflow-x-auto shadow-md rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <table class="w-full text-sm text-left text-gray-400">
+      <thead class="text-xs uppercase bg-gray-700 text-gray-400">
         <tr>
           <th class="px-3 py-3" scope="col">
             Name
           </th>
-
-          <th class="hidden md:flex py-3" scope="col">
+          <th class="hidden md:table-cell py-3" scope="col">
             Beschreibung
           </th>
-
           <th class="py-3" scope="col">
             Startzeit
           </th>
-
-          <th class="hidden lg:flex py-3" scope="col">
+          <th class="hidden lg:table-cell py-3" scope="col">
             Endzeit
           </th>
-
+          <th class="hidden sm:table-cell py-3" scope="col">
+            Laufzeit
+          </th>
           <th class="py-3" scope="col">
             Status
           </th>
-
-          <th class="hidden sm:flex py-3" scope="col">
+          <th class="hidden sm:table-cell py-3" scope="col">
             Ersteller
           </th>
-
         </tr>
       </thead>
       <tbody>
