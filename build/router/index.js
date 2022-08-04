f2565a90c7322ff3c4f08b7d116e340da6d601e6
bibaltik <bibaltik@exdrals.de>
2022-08-04 19:43:09 +0200

    Style fix

--- build/router/index.js
+++ build/router/index.js
@@ -15,54 +15,100 @@ import EventCreate from '@/views/event/EventCreate';
 import ProjectView from "@/views/ProjectView";
 
 const routes = [{
-  component: MainLayout, path: "/", children: [{
-    path: "/home", name: "home", alias: '/', component: MainView,
-  }, {
-    path: "/login", name: "login", component: LoginView,
-  }, {
-    path: "/logout", name: "logout", component: LogoutView,
-  },{
-    path: "/register", name: "register", component: RegisterView,
-  },{
-    path: "/user/:uuid", name: "user_entry", component: UserView,
-  }, {
-    path: "/event/information", name: "event_general_information", component: EventAbout,
-  }, {
-    path: "/event/:id", name: "event_entry", component: EventEntry,
-  }, {
-    path: "/event/list", name: "event_list", component: EventList,
-  }, {
-    path: "/event/create", name: "event_create", component: EventCreate, meta: {requireAuth: true},
-  }, {
-    path: "/project/:uuid", name: "project_entry", component: ProjectView,
-  }, {
-    path: "/about", name: "about", component: MainView,
-  }, {
-    path: "/discord", name: "discord", beforeEnter() {
+  component: MainLayout,
+  path: "/",
+  children: [{
+    path: "/home",
+    name: "home",
+    alias: '/',
+    component: MainView,
+  },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/logout",
+      name: "logout",
+      component: LogoutView,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterView,
+    },
+    {
+      path: "/user/:uuid",
+      name: "user_entry",
+      component: UserView,
+    }, {
+      path: "/event/information",
+      name: "event_general_information",
+      component: EventAbout,
+    },
+    {
+      path: "/event/:id",
+      name: "event_entry",
+      component: EventEntry,
+    },
+    {
+      path: "/event/list",
+      name: "event_list",
+      component: EventList,
+    },
+    {
+      path: "/event/create",
+      name: "event_create",
+      component: EventCreate,
+      meta: {
+        requireAuth: true
+      },
+    },
+    {
+      path: "/project/:uuid",
+      name: "project_entry",
+      component: ProjectView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: MainView,
+    },
+  ]
+},
+  {
+    path: "/discord",
+    name: "discord", beforeEnter() {
       location.href = 'https://discord.gg/VjrfCFKRgR';
     },
-  }, {
-    path: '/error', name: 'error', component: ErrorView,
-  }, {
-    path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundView,
-  },],
-},];
+  },
+  {
+    path: '/error',
+    name: 'error',
+    component: ErrorView,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: NotFoundView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL), routes,
 });
 
 
-
 router.beforeEach((to, from, next) => {
   const user = useUserService();
 
   if (to.meta.requireAuth && !user.isAuthenticated()) {
-    return next({ path: "/login" });
+    return next({path: "/login"});
   }
 
   if (to.path === "/login" && localStorage.getItem("token") !== null) {
-    return next({ path: "/" });
+    return next({path: "/"});
   }
 
   return next();
