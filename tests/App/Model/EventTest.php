e9bad29647e8f697218755396a9d4ffa3cac7922
BibaltiK <github@exdrals.de>
2022-10-16 23:51:48 +0200

    refactor EventTest

--- tests/App/Model/EventTest.php
+++ tests/App/Model/EventTest.php
@@ -2,6 +2,7 @@
 
 namespace AppTest\Model;
 
+use App\Enum\DateTimeFormat;
 use App\Enum\EventStatus;
 use App\Model\Event;
 use DateTime;
@@ -9,8 +10,14 @@ use PHPUnit\Framework\TestCase;
 
 class EventTest extends TestCase
 {
+    private const TEST_USER_ID = 1;
+    private const TEST_TIME = '1000-01-01 00:00';
+    private const TEST_TITLE = 'Test Title';
+    private const TEST_DESCRIPTION = 'Test Description';
+    private const TEST_EVENT_TEXT = 'Test Event Text';
+    private const TEST_DURATION = 1;
+
     private Event $event;
-    private string $testTime = '1000-01-01 00:00:00';
 
     protected function setUp(): void
     {
@@ -21,32 +28,32 @@ class EventTest extends TestCase
 
     public function testCanSetAndGetId(): void
     {
-        $eventId = $this->event->setId(1);
+        $eventId = $this->event->setId(self::TEST_USER_ID);
         $id = $eventId->getId();
 
         $this->assertInstanceOf(Event::class, $eventId);
         $this->assertIsInt($id);
-        $this->assertSame(1, $id);
+        $this->assertSame(self::TEST_USER_ID, $id);
     }
 
     public function testCanSetAndGetUserId(): void
     {
-        $eventUserId = $this->event->setUserId(1);
+        $eventUserId = $this->event->setUserId(self::TEST_USER_ID);
         $userId = $eventUserId->getUserId();
 
         $this->assertInstanceOf(Event::class, $eventUserId);
         $this->assertIsInt($userId);
-        $this->assertSame(1, $userId);
+        $this->assertSame(self::TEST_USER_ID, $userId);
     }
 
     public function testCanSetAndGetName(): void
     {
-        $eventName = $this->event->setTitle('test');
+        $eventName = $this->event->setTitle(self::TEST_TITLE);
         $name = $eventName->getTitle();
 
         $this->assertInstanceOf(Event::class, $eventName);
         $this->assertIsString($name);
-        $this->assertSame('test', $name);
+        $this->assertSame(self::TEST_TITLE, $name);
     }
 
     public function testCanSetAndGetDescription(): void
@@ -55,52 +62,52 @@ class EventTest extends TestCase
 
         $this->assertNull($description);
 
-        $eventDescription = $this->event->setDescription('test');
+        $eventDescription = $this->event->setDescription(self::TEST_DESCRIPTION);
         $description = $eventDescription->getDescription();
 
         $this->assertInstanceOf(Event::class, $eventDescription);
         $this->assertIsString($description);
-        $this->assertSame('test', $description);
+        $this->assertSame(self::TEST_DESCRIPTION, $description);
     }
 
     public function testCanSetAndGetEventText(): void
     {
-        $eventEventText = $this->event->setEventText('test');
+        $eventEventText = $this->event->setEventText(self::TEST_EVENT_TEXT);
         $eventText = $eventEventText->getEventText();
 
         $this->assertInstanceOf(Event::class, $eventEventText);
         $this->assertIsString($eventText);
-        $this->assertSame('test', $eventText);
+        $this->assertSame(self::TEST_EVENT_TEXT, $eventText);
     }
 
     public function testCanSetAndGetCreateTime(): void
     {
-        $eventCreateTime = $this->event->setCreateTime(new DateTime($this->testTime));
+        $eventCreateTime = $this->event->setCreateTime(new DateTime(self::TEST_TIME));
         $createTime = $eventCreateTime->getCreateTime();
 
         $this->assertInstanceOf(Event::class, $eventCreateTime);
         $this->assertInstanceOf(DateTime::class, $createTime);
-        $this->assertSame($this->testTime, $createTime->format('Y-m-d H:i:s'));
+        $this->assertSame(self::TEST_TIME, $createTime->format(DateTimeFormat::ISO_8601->value));
     }
 
     public function testCanSetAndGetStartTime(): void
     {
-        $eventStartTime = $this->event->setStartTime(new DateTime($this->testTime));
+        $eventStartTime = $this->event->setStartTime(new DateTime(self::TEST_TIME));
         $startTime = $eventStartTime->getStartTime();
 
         $this->assertInstanceOf(Event::class, $eventStartTime);
         $this->assertInstanceOf(DateTime::class, $startTime);
-        $this->assertSame($this->testTime, $startTime->format('Y-m-d H:i:s'));
+        $this->assertSame(self::TEST_TIME, $startTime->format(DateTimeFormat::ISO_8601->value));
     }
 
     public function testCanSetAndGetDuration(): void
     {
-        $eventDuration = $this->event->setDuration(1);
+        $eventDuration = $this->event->setDuration(self::TEST_DURATION);
         $duration = $eventDuration->getDuration();
 
         $this->assertInstanceOf(Event::class, $eventDuration);
         $this->assertIsInt($duration);
-        $this->assertSame(1, $duration);
+        $this->assertSame(self::TEST_DURATION, $duration);
     }
 
     public function testCanSetAndGetActive(): void
