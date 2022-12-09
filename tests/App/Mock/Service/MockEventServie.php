bdb3f924634fb6bede6607032987de2da9a56c84
BibaltiK <bibaltik@exdrals.de>
2022-12-10 00:41:12 +0100

    add Test for EventMiddleware

--- tests/App/Mock/Service/MockEventServie.php
+++ tests/App/Mock/Service/MockEventServie.php
@@ -6,6 +6,8 @@ use App\Hydrator\ReflectionHydrator;
 use App\Model\Event;
 use App\Service\EventService;
 use App\Test\Mock\Table\MockEventTable;
+use Fig\Http\Message\StatusCodeInterface as HTTP;
+use Psr\Log\InvalidArgumentException;
 
 class MockEventServie extends EventService
 {
@@ -17,5 +19,14 @@ class MockEventServie extends EventService
     public function create(Event $event): bool
     {
         return $event->getId() === 2;
+    }
+
+    public function findById(int $id): Event
+    {
+        if ($id === 1) {
+            return new Event();
+        }
+
+        throw new InvalidArgumentException('Could not find Event', HTTP::STATUS_BAD_REQUEST);
     }
 }
