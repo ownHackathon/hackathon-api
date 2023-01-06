d35278755e7c5ba07d04e616f2155e04f49df4e1
BibaltiK <bibaltik@exdrals.de>
2023-01-06 10:38:20 +0100

    Route not Found was Handle wihth Indexhandler

--- src/App/Handler/IndexHandler.php
+++ src/App/Handler/IndexHandler.php
@@ -3,9 +3,11 @@
 namespace App\Handler;
 
 use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Fig\Http\Message\StatusCodeInterface as HTTP;
 
 use function file_get_contents;
 
@@ -13,7 +15,12 @@ class IndexHandler implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $indexFile = file_get_contents(ROOT_DIR . 'public/default.mustache');
-        return new HtmlResponse($indexFile);
+        $indexFile = ROOT_DIR . 'public/default.mustache';
+
+        if (file_exists($indexFile)) {
+            return new HtmlResponse(file_get_contents($indexFile));
+        }
+
+        return new JsonResponse('', HTTP::STATUS_NO_CONTENT);
     }
 }
