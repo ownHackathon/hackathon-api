6209b4d41b7feacee0e64a3248b8e32898b2c6ab
BibaltiK <bibaltik@exdrals.de>
2023-02-05 00:13:43 +0100

    add go Back Button

--- client/src/views/topic/TopicList.vue
+++ client/src/views/topic/TopicList.vue
@@ -1,9 +1,17 @@
 <template>
+  <div class="mt-5 mb-2">
+    <button class="button" @click="goBack">
+      zurück
+    </button>
+  </div>
   <div v-if="loading">
     <TheSpinner/>
   </div>
+  <div v-if="error" class="center">
+    <span>Es ist ein Fehler aufgetreten: {{error}}</span>
+  </div>
   <div id="content">
-    <div class="relative overflow-x-auto shadow-md rounded-lg mt-5">
+    <div class="relative overflow-x-auto shadow-md rounded-lg">
       <table class="w-full text-sm text-left text-gray-400">
         <thead class="text-xs uppercase bg-gray-700 text-gray-400">
         <tr>
@@ -27,6 +35,11 @@
       </table>
     </div>
   </div>
+  <div class="mt-2 mb-2">
+    <button class="button" @click="goBack">
+      zurück
+    </button>
+  </div>
 </template>
 
 <script setup>
@@ -35,15 +48,20 @@ import TopicListEntry from "@/views/topic/componets/TopicListEntry.vue";
 import {storeToRefs} from "pinia";
 import {useTopicStore} from "@/store/topic";
 import {onMounted} from "vue";
+import {useRouter} from "vue-router";
 
 const topicStore = useTopicStore();
-const {topics, loading} = storeToRefs(topicStore);
-
+const {topics, loading, error} = storeToRefs(topicStore);
+const router = useRouter();
 
 onMounted(() => {
       topicStore.fetchAvailableTopics();
     }
 );
+
+function goBack() {
+  return router.go(-1)
+}
 </script>
 
 <style scoped>
