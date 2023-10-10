47e4a260fb0e485f40cf0480736039539f35c29f
BibaltiK <bibaltik@exdrals.de>
2023-10-10 21:55:29 +0200

    add openapi for logout

--- src/Authentication/Handler/LogoutHandler.php
+++ src/Authentication/Handler/LogoutHandler.php
@@ -6,11 +6,23 @@ use Laminas\Diactoros\Response\RedirectResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use OpenApi\Attributes as OA;
+use Fig\Http\Message\StatusCodeInterface as HTTP;
 
 class LogoutHandler implements RequestHandlerInterface
 {
+    #[OA\Get(
+        path: '/api/logout',
+        tags: ['User Control'],
+        responses: [
+            new OA\Response(
+                response: HTTP::STATUS_SEE_OTHER,
+                description: 'not implementet yet'
+            )
+        ]
+    )]
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new RedirectResponse('/', 303);
+        return new RedirectResponse('/', HTTP::STATUS_SEE_OTHER);
     }
 }
