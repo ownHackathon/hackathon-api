a59e612fb4138dc9285af113b9774396b0e4f3f4
BibaltiK <bibaltik@exdrals.de>
2023-12-27 21:21:01 +0100

    adjust documentation

--- src/App/Handler/EventListHandler.php
+++ src/App/Handler/EventListHandler.php
@@ -16,25 +16,27 @@ readonly class EventListHandler implements RequestHandlerInterface
 {
     #[OA\Get(path: '/api/event', summary: 'List of all events', tags: ['Event'])]
     #[OA\QueryParameter(
-        name: 'sort',
-        description: 'determines the display order of the events',
+        name: 'order',
+        description: 'The field to be sorted by<br>
+                      Options: ID|OWNER|TITLE|DESCRIPTION|DURATION|STARTTIME|STATUS',
         in: 'query',
         required: false,
         schema: new OA\Schema(type: 'string'),
-        example: 'desc'
+        example: 'startTime',
     )]
     #[OA\QueryParameter(
-        name: 'order',
-        description: 'The field to be sorted by',
+        name: 'sort',
+        description: 'determines the display order of the events<br>
+                      Options: ASC|DESC',
         in: 'query',
         required: false,
         schema: new OA\Schema(type: 'string'),
-        example: 'startTime'
+        example: 'DESC',
     )]
     #[OA\Response(
         response: HTTP::STATUS_OK,
         description: 'Success',
-        content: new OA\JsonContent(type: 'array', items: new OA\Items(ref: EventDto::class))
+        content: new OA\JsonContent(type: 'array', items: new OA\Items(ref: EventDto::class)),
     )]
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
