62044562ceac7aac8f181f30f0099065c9c6273f
BibaltiK <bibaltik@exdrals.de>
2025-08-14 23:52:20 +0200

    add version to constants

--- constants.php
+++ constants.php
@@ -1,3 +1,5 @@
 <?php declare(strict_types=1);
+
 define('ROOT_DIR', realpath(__DIR__) . '/');
-define('CONFIG_DIR', ROOT_DIR . 'config/');
+const CONFIG_DIR = ROOT_DIR . 'config/';
+const PROJECT_VERSION = 'v0.1.0';
