416c850e90b7b961ff528bd1c5718102da3d429f
BibaltiK <bibaltik@exdrals.de>
2026-02-04 22:53:40 +0100

    test: remove backup test files and obsolete test classes

--- tests_backup/UnitTest/Mock/Service/MockClientIdentificationService.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace UnitTest\Mock\Service;
-
-use Exdrals\Identity\Infrastructure\Service\ClientIdentification\ClientIdentificationService;
-
-readonly class MockClientIdentificationService extends ClientIdentificationService
-{
-}
